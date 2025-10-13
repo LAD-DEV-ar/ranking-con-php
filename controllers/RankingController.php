c7e031f85ecccaee99c81441fdf47c14edc75b16
LAD-DEV-ar <eliasaarroyo@gmail.com>
2025-10-12 22:18:23 -0300

    Los archivos que faltaban del MVC

---
+++ controllers/RankingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+
+class RankingController{
+    public static function index(Router $router){
+
+        $router->render('main/index', [
+            "titulo" => "ScoreBoard"
+        ]);
+    }
+}
+
+
--- includes/app.php
+++ includes/app.php
@@ -4,6 +4,7 @@ require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
 
+
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
