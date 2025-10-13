a0d25ba0880da7786694952e8a5822362789d0f1
LAD-DEV-ar <eliasaarroyo@gmail.com>
2025-10-12 22:12:13 -0300

    Seguimos avanzando: agregamos los principal de la arquitectura MVC

--- public/index.php
+++ public/index.php
@@ -3,8 +3,11 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\RankingController;
 
 $router = new Router();
+
+$router->get('/', [RankingController::class, 'index']);
 
 
 
